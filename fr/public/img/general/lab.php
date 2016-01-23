0dd2a30efc04f57e25dce86cfdfe6c66f11b077b
Calem Bendell <calem.j.bendell@gmail.com>
2016-01-22 17:45:39 -0800

    builds

---
+++ fr/public/img/general/lab.php
@@ -0,0 +1,243 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<!--
+Copyright: Darren Hester 2006, http://www.designsbydarren.com
+License: Released Under the "Creative Commons License", 
+http://creativecommons.org/licenses/by-nc/2.5/
+-->
+
+<head>
+
+<!-- Meta Data -->
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=ISO-8859-1">
+<meta name="description" content="Free 2-Column CSS Web Design Template" />
+<meta name="keywords" content="Free, 2-Column, CSS, Web, Design, Template" />
+
+<!-- Site Title -->
+<title>Be A Computer Scientist for a Week | McGill Computer Science Summer Camp 2010 Edition</title>
+
+<script type="text/javascript" src="swfobject.js"></script>
+<script type="text/javascript" src="thickbox/jquery.js"></script>
+<script type="text/javascript" src="thickbox/thickbox.js"></script>
+
+<!-- Link to Style External Sheet -->
+<link href="css/style.css" type="text/css" rel="stylesheet" />
+<link href="thickbox/thickbox.css" type="text/css" rel="stylesheet" media="screen" />
+
+</head>
+
+<body>
+
+<div id="page_wrapper">
+
+<div id="page_header">
+
+<h1>Be A Computer Scientist for a Week</h1>
+<h2>McGill Computer Science Summer Camp 2014</h2>
+<h2>July 7-11 2014</h2>
+<h2>Registration is Open until May 16 2014</h2>
+
+</div>
+
+<div id="menu_bar_fr">
+
+<table style="width:760px;">
+
+<tr>
+
+<td style="background: black; color: white;">
+MENU:
+</td>
+<td>
+<ul>
+<li><a href="english.php">Welcome</a></li>
+<li><a href="registration_en.php">Registration</a></li>
+<li><a href="contactus_en.php">Contact Us</a></li>
+<li><a href="sponsors_en.php">Sponsors</a></li>
+<li><a href="francais.php">Francais</a></li>
+</ul>
+</td>
+</tr>
+
+<tr>
+<td style="background: black; color: white;">
+CAMPS:
+</td>
+<td> 
+<ul>
+<li><a href="missionmars_en.php">AI and Robotics</a></li>
+<li><a href="gamecamp_en.php">Game Guru</a></li>
+<li><a href="medical_en.php"><p style="text-decoration:line-through;">Medical Computing</p></a></li>
+<li><a href="climate_en.php">Climate Change</a></li>
+</td>
+</ul>
+
+</tr>
+
+</table>
+
+</div>
+
+<br></br>
+
+<div id="content_wrapper">
+
+<div id="left_side">
+
+<!--
+<font color="red">Registration extended until May 25th!</font>
+-->
+
+
+<h3>McGill Computer Science Summer Camp <br/>2010 Edition</h3>
+
+
+<h4>Picture Book</h4>
+
+<p>
+
+<a href="img/2010/Lab1.jpg" class="thickbox" rel="2010"><img src="img/2010/Lab1.jpg" width="200" /></a>
+<a href="img/2010/Lab2.jpg" class="thickbox" rel="2010"><img src="img/2010/Lab2.jpg" width="200" /></a>
+<a href="img/2010/Lab3.jpg" class="thickbox" rel="2010"><img src="img/2010/Lab3.jpg" width="200" /></a>
+<a href="img/2010/Lab4.jpg" class="thickbox" rel="2010"><img src="img/2010/Lab4.jpg" width="200" /></a>
+<a href="img/2010/Lab5.jpg" class="thickbox" rel="2010"><img src="img/2010/Lab5.jpg" width="200" /></a>
+<a href="img/2010/Lab6.jpg" class="thickbox" rel="2010"><img src="img/2010/Lab6.jpg" width="200" /></a>
+<a href="img/2010/Lab7.jpg" class="thickbox" rel="2010"><img src="img/2010/Lab7.jpg" width="200" /></a>
+<a href="img/2010/Lab8.jpg" class="thickbox" rel="2010"><img src="img/2010/Lab8.jpg" width="200" /></a>
+<a href="img/2010/Lect1.jpg" class="thickbox" rel="2010"><img src="img/2010/Lect1.jpg" width="200" /></a>
+<a href="img/2010/Lect2.jpg" class="thickbox" rel="2010"><img src="img/2010/Lect2.jpg" width="200" /></a>
+<a href="img/2010/Lect3.jpg" class="thickbox" rel="2010"><img src="img/2010/Lect3.jpg" width="200" /></a>
+<a href="img/2010/Lect4.jpg" class="thickbox" rel="2010"><img src="img/2010/Lect4.jpg" width="200" /></a>
+<a href="img/2010/CSA_1.jpg" class="thickbox" rel="2010"><img src="img/2010/CSA_1.jpg" width="200" /></a>
+<a href="img/2010/CSA_2.jpg" class="thickbox" rel="2010"><img src="img/2010/CSA_2.jpg" width="200" /></a>
+<a href="img/2010/CSA_3.jpg" class="thickbox" rel="2010"><img src="img/2010/CSA_3.jpg" width="200" /></a>
+</p>
+
+<p>Don't forget to check out the <a href="#">video</a> recap.</p>
+
+<h4>Camp Staff</h4>
+
+<ul class="arrow_list">
+  <li>Camp Director : Joseph Vybihal </li>
+  <li>Administration : Jonathan Cottrell and Mona Ghassemi </li>
+  <li>Tech Admin : Milena Scaccia and Jonathan Cottrell</li>
+  <li>AI Camp Director : Milena Scaccia </li>
+  <li>Game Camp Director : Joseph Vybihal </li>
+  <li>Medical Camp Director : Mathieu Blanchette </li>
+  <li>Primary Volunteers: Mathieu Rousseau, Mathieu Lavallée-Adam, Rae Price, Nick Parenteau, Wei Quan, Roy Wang, and Santiago Paiva</li>
+  <li>Professors: Gregory Dudek, Ioannis Rekleitis and Kaleem Siddiqi</li>
+</ul>
+
+<h4>Sponsors</h4>
+
+<p>
+Great thanks to our sponsors for making the 2010 edition possible.
+</p>
+
+<ul class="arrow_list">
+<li><a href="http://www.asc-csa.gc.ca">Canadian Space Agency</a></li>
+<li><a href="http://www.ea.com">Electronic Arts</a></li>
+<li><a href="http://www.a2m.com">A2M</a></li>
+<li><a href="http://www.provigo.ca/">Provigo</a></li>
+<li><a href="http://www.cs.mcgill.ca/">McGill School of Computer Science</a></li>
+</ul>
+
+
+</div>
+
+<div id="right_side">
+
+<p>Find us on Youtube and Facebook!</p>
+<a href="http://www.new.facebook.com/home.php#/group.php?gid=27305958106" target="_new"><img class="photo-left" width="30" height="30" src="img/facebook.jpg"></a>
+<a href="http://www.youtube.com/user/McGillSummerCamp" target="_new"><img class="photo-left" width="30" height="30" src="img/youtube.jpg"></a>
+
+<div id="spacer"></div>
+
+<h3>Information</h3>
+
+<ul class="arrow_list">
+<li><a href="students_en.php">For students</a></li>
+<li><a href="schools_en.php">For schools</a></li>
+<li><a href="sponsors_en.php">For sponsors</a></li>
+<li><a href="map_en.php">Where is the camp?</a></li>
+<li><a href="organizers_en.php">Organizers</a></li>
+<li><a href="visits_en.php">Request a visit</a></li>
+</ul>
+
+<h3>Important Doc.</h3>
+
+<p>
+The following documents are available in PDF format.
+</p>
+
+<ul class="arrow_list">
+<li><a href="docs/Poster2014.png">Poster</a> (<a href="docs/Poster2014.jpg">Smaller</a>)</li>
+<li><a href="docs/Schedule 2014.pdf">Schedule</a></li>
+<li><a href="docs/English letter 2014.pdf">Letter of invitation</a></li>
+<li><a href="docs/CampusMap.pdf">McGill Campus</a></li>
+</ul>
+
+<p>
+These documents are also available in <a href="francais.php">French</a>.
+</p>
+
+<h3>Past Camps</h3>
+<ul class="arrow_list">
+<li><a href="pastcamp_en2010.php">2010</a></li>
+<li><a href="pastcamp_en2009.php">2009</a></li>
+<li><a href="pastcamp_en2008.php">2008</a></li>
+<li><a href="pastcamp_en2007.php">2007</a></li>
+<li><a href="pastcamp_en2006.php">2006</a></li>
+<li><a href="pastcamp_en2005.php">2005</a></li>
+</ul>
+
+<h3>Sponsors 2014</h3>
+
+<a href="http://www.ea.com">
+<img src="img/sponsors/ea_logo.png" width="150" />
+</a>
+
+<a href="http://www.hydroquebec.qc.ca">
+<img src="img/2014/logo_ireq.gif" width="150" />
+</a>
+
+<a href="http://www.ludia.com">
+<img src="img/2014/ludia.jpg" width="150" />
+</a>
+
+<a href="http://www.morganstanley.com">
+<img src="img/2014/morgan_stanley_logo.jpg" width="150" />
+</a>
+
+<a href="http://www.wbgames.com">
+<img src="img/2014/WB_Games_Montreal.png" width="150" />
+</a>
+
+<a href="http://www.robotshop.com">
+<img src="img/2014/RobotShop.jpg" width="150" />
+</a>
+
+<a href="http://www.mcgill.ca">
+<img src="img/sponsors/mcgill_logo.png" width="150" />
+</a>
+
+</div>
+
+<div id="spacer"></div>
+
+</div>
+
+<div id="page_footer">
+
+<p>&copy; 2006 Darren Hester, DesignsByDarren.com, Some Rights Reserved.<br /></p>
+
+</div>
+
+</div>
+
+</body>
+
+</html>
+
